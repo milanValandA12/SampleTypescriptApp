5a9299489b4fdf4caffa8df6cc140c37af430fee
Milan <milankumar.valand@alshaya.com>
2022-07-01 16:36:22 +0530

    added translation

--- src/screens/auth/login.screen.tsx
+++ src/screens/auth/login.screen.tsx
@@ -9,20 +9,20 @@ const LoginScreen = () => {
   const {username, setUsername, getProfile, fetching} = useGetUserDetails();
   return (
     <View style={styles.screen}>
-      <AppTypography>githubUsername</AppTypography>
+      <AppTypography>Enter Github Username</AppTypography>
       <AppTextInput
         onChangeText={text => setUsername(text)}
-        placeholder="username"
+        placeholder="Username"
         value={username}
       />
-      <AppButton title="SignIButton" textKey='getDetails' loading={fetching} onPress={getProfile} />
+      <AppButton title="SignIButton" textKey='Get Details' loading={fetching} onPress={getProfile} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    marginTop: 50,
+    marginTop: 120,
     padding: 8,
   },
 });
